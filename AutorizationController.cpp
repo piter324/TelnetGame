17a8cf9064ed385bc71b89703d6c8d497c8bb5a8
Unknown <bilewicz.kar@gmail.com>
2018-12-17 13:49:13 +0100

    register function added

--- AutorizationController.cpp
+++ AutorizationController.cpp
@@ -1,5 +1,3 @@
-
-
 #include "AutorizationController.h"
 
 /* checks the existence of the user file */ 
@@ -54,5 +52,50 @@ bool AutorizationController::logOut(std::string username) {
         return true;
     }
     std::cout << "niezalogowany " << username << std::endl;
+    return false;
+}
+
+//untested!!!
+bool AutorizationController::registerUser(std::string username, std::string passw) {
+    // check if this username exists
+    std::ifstream dbFile;
+    dbFile.open(pathToUserDatabase_);
+    bool userFound = false;
+    std::cout << "username = " << username << " ;password " << passw << std::endl;
+    if (dbFile) {
+        std::string userInfo;
+        while (!userFound && getline (dbFile,userInfo) ) {
+           // std::cout << "szukamy... " << userInfo.length() <<" " << username.length() << " " << passw.length() << std::endl;
+           //     std::cout << "kandydat... " << std::endl;
+                std::size_t posUsername = userInfo.find(username);
+                std::size_t posSeparator = userInfo.find(":");
+                std::cout << "posUsername = " << posUsername << " ;posSeparator " << posSeparator
+                << " len = " << username.length() << std::endl;
+                std::cout <<  
+                if(posUsername == 0 && username.length() = posSeparator) {
+                    userFound = true;
+                }
+            }
+    }
+    dbFile.close();
+    if (userFound) {
+        std::cout << "Taki użytkownik już istnieje" << std::endl;
+        return false;
+    }
+
+    //content preparing
+    std::string content = username + ":" + passw + "\n";
+    std::cout << "Dajemy content: " << content; 
+
+    //MUTEX?
+    std::ofstream dbFile_app;
+    dbFile_app.open(pathToUserDatabase_, std::ios_base::app);
+    if (dbFile_app) {
+        dbFile_app << content;
+        return true;
+    }
+    
+    dbFile_app.close();
+
     return false;
 }
--- AutorizationController.h
+++ AutorizationController.h
@@ -24,6 +24,7 @@ class AutorizationController {
         bool isLoggedIn(std::string username);
         bool logIn(std::string username, std::string passw);
         bool logOut(std::string username);
+        bool registerUser(std::string username, std::string passw);
     private:
         std::string pathToUserDatabase_;
         std::string pathToUserFolder_;
